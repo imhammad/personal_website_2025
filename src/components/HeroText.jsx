d80170378211dea68634a010f74543f067857119
Hammad Hassan <hammadhassan319@gmail.com>
2025-07-27 17:46:07 +0800

    hero_section_1

---
+++ src/components/HeroText.jsx
@@ -0,0 +1,26 @@
+const HeroText = () => {
+  return (
+    <div className="z-10 mt-20 text-center md:mt-40 md:text-left
+    rounded-3xl bg-clip-text">
+      {/* Desktop View */}
+      <div className="flex-col hidden md:flex c-space">
+        <h1 className="text-4xl font-medium">Hi, I'm Hammad</h1>
+        <div className="flex flex-col items-start">
+            <p className="text-5xl font-medium text-neutral-300 ">
+                A Programmar <br /> Dedicated to Solving
+            </p>
+            <div>
+                
+            </div>
+
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default HeroText
+
+
+
