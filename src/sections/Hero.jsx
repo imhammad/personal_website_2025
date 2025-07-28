43d5972b7987bba3e7750e9a2f2e92f0a5c5bcfe
Hammad Hassan <hammadhassan319@gmail.com>
2025-07-28 23:28:44 +0800

    spaceman_added

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,15 +1,55 @@
+import { useMediaQuery } from 'react-responsive';
+import { Astronaut } from '../components/Astronaut';
 import HeroText from '../components/HeroText';
 import ParallaxBackground from '../components/ParallaxBackground';
+import { Suspense } from 'react';
+import { Float, OrbitControls } from '@react-three/drei';
+import Loader from '../components/Loader';
+import { easing } from "maath";
+import { Canvas, useFrame} from "@react-three/fiber";
 
 
 const Hero = () => {
+    const isMobile = useMediaQuery({ maxWidth: 853});
   return (
     <section className="flex items-start justify-center md:items-start
     md:justify-start min-h-screen overflow-hidden c-space">
       <HeroText />
       <ParallaxBackground />
+      <figure 
+      className="absolute inset-0"
+      style={{ width: "100vw", height: "100vh"}}
+      >
+      
+      <Canvas camera={{ position: [0, 1, 3] }}>
+        <Suspense fallback={<Loader />}>
+            <Float>
+                <Astronaut
+                scale={isMobile && 0.23}
+                position={isMobile && [0, -1.5, 0]}
+                />
+
+            </Float>
+            <Rig />
+        </Suspense>
+        <OrbitControls enableZoom={false} />
+      </Canvas>
+      </figure>
+
+
     </section>
   )
+}
+
+function Rig(){
+    return useFrame((state, delta) => {
+        easing.damp3(
+            state.camera.position,
+            [state.mouse.x / 10, 1 + state.mouse.y / 10, 3],
+            0.5,
+            delta
+        )
+    })
 }
 
 export default Hero;
